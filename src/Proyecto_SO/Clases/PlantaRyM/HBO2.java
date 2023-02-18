fa040840acca48f7b81480aba33146dc7d75b564
Gabriella Suarez <g.suarez@correo.unimet.edu.ve>
2023-02-18 16:51:31 -0400

    empezando a poner los limites y viendo qlq con los productores

--- src/Proyecto_SO/Clases/PlantaRyM/HBO2.java
+++ src/Proyecto_SO/Clases/PlantaRyM/HBO2.java
@@ -12,4 +12,37 @@ import java.util.concurrent.Semaphore;
  */
 public class HBO2 {
     
+    // Limites empleados
+    public static int cantidadMaxProductores; 
+    public static int cantidadMaxProjectManager;
+    public static int cantidadMaxEnsambladores;
+    public static int cantidadDirectores;
+    
+    // Tiempo
+    public static int duracionDia;
+    
+    // Numero de partes de episodio 
+    public static int cantidadIntro;
+    public static int cantidadInicio;
+    public static int cantidadPTwist;
+    public static int cantidadCierre;
+    public static int cantidadCreditos;
+    public static int cantidadMaxIntro;
+    public static int cantidadMaxInicio;
+    public static int cantidadMaxPTwist;
+    public static int cantidadMaxCierre;
+    public static int cantidadMaxCreditos;
+    
+    //Cantidad maxima de Productores
+    
+    public static int cantidadProdIntro; 
+    public static int cantidadProdInicio; 
+    public static int cantidadProdPTwist; 
+    public static int cantidadProdCierre; 
+    public static int cantidadProdCreditos; 
+    
+    // Semaforos de Project Manager y Director
+    
+    
+    
 }
--- src/Proyecto_SO/Clases/PlantaRyM/Productor.java
+++ src/Proyecto_SO/Clases/PlantaRyM/Productor.java
@@ -16,11 +16,13 @@ public abstract class Productor extends Thread {
     protected Semaphore x;
     protected Semaphore ensamblador;
     protected Semaphore productor;
+    protected Semaphore activo; 
 
-    public Productor(Semaphore x, Semaphore ensamblador, Semaphore productor) {
+    public Productor(Semaphore x, Semaphore ensamblador, Semaphore productor, Semaphore activo) {
         this.x = x;
         this.ensamblador = ensamblador;
         this.productor = productor;
+        this.activo = activo; 
 
     }
     
